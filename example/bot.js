3a3f45e0d4da7498a8d9752f3f1564027209c874
Feldwor <BoQsc@users.noreply.github.com>
2022-11-29 12:16:17 +0200

    Update bot.js

--- example/bot.js
+++ example/bot.js
@@ -24,6 +24,14 @@ client.on("ready", async () => {
 		console.info(`Servers joined:  ${client.servers.size}!`);
 		console.info(`Get server by ID:  ${client.servers.get("01GJ0CVB1ZV2H1YA13S6ZK4N4N").name}!`);
 		
+		console.info(`Servers owning:  ${client.servers.forEach(
+		(values, keys, objects) => {
+			console.info("Servers:");
+			console.info(" " + values._id);
+			
+		}
+		
+		)}!`);
 		/*console.info(`Servers owning:  ${client.servers.forEach(client.servers)}!`);*/
 		
 		/* console.dir(client.servers.data_, { depth:1 }) */
