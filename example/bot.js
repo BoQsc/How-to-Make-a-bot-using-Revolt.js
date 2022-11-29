af256036129cf1635dfc6527614558f3f80ad2f8
Feldwor <BoQsc@users.noreply.github.com>
2022-11-29 11:34:05 +0200

    Create bot.js

---
+++ example/bot.js
@@ -0,0 +1,42 @@
+const { Client } = require("revolt.js");
+
+let client = new Client();
+
+console.info(`Welcome to a Basic Revolt Bot.`);
+console.info(`Revolt Library version: ${require("revolt.js").LIBRARY_VERSION}!`);
+console.info(`_______________________________`);
+
+client.on("ready", async () => {
+		console.info(`Bot Token: ${client.session}!`);
+		console.info(`User ID: ${client.user._id}!`);
+		console.info(`Logged in as ${client.user.username}!`);
+		console.info(`Bot online: ${client.user.online}!`);
+        client.users.edit({
+            status: {
+                text: "Listening to you.",
+                presence: "Idle",
+            },
+        });
+		console.info(`Bot owner ${client.user.bot.owner}!`); 
+		console.info(`Created at ${client.user.createdAt}!`);
+		console.info(`Presence: ${client.user.status.presence}!`);
+		console.info(`Presence text: Logged in as ${client.user.status.text}!`);
+		console.info(`Servers joined:  ${client.servers.size}!`);
+		console.info(`Get server by ID:  ${client.servers.get("01GJ0CVB1ZV2H1YA13S6ZK4N4N").name}!`);
+		
+		/*console.info(`Servers owning:  ${client.servers.forEach(client.servers)}!`);*/
+		
+		/* console.dir(client.servers.data_, { depth:1 }) */
+		/* console.dir(client.servers, { depth:1 }) */
+		/* console.dir(client.channels, { depth:1 }) */
+
+	}
+);
+
+client.on("message", async (message) => {
+    if (message.content === "hello") {
+        message.channel.sendMessage("world");
+    }
+});
+
+client.loginBot("..");
