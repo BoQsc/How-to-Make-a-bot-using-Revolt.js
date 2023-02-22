4db09cdb2c78baea740443e579da0b43a416ed02
Feldwor <BoQsc@users.noreply.github.com>
2023-02-22 15:06:44 +0200

    Add files via upload

---
+++ Automated setup for Revolt Bot development./bot.js
@@ -0,0 +1,46 @@
+// This is a bot.js example.
+const { Client } = require("revolt.js");
+
+let client = new Client();
+
+client.on("ready", async () => {
+	console.info(`User ID: ${client.user._id}!`);
+	console.info(`Logged in as ${client.user.username}!`);
+	console.info(`Bot online: ${client.user.online}!`);
+	
+	client.users.edit({
+		status: {
+			text: "Listening to you.",
+			presence: "Idle",
+		},
+	});
+});
+
+client.on("message", async (message) => {
+	if (message.content === "hello") {
+		message.channel.sendMessage("world");
+	}
+});
+
+let bot_token = process.argv[2];
+
+var fs = require('fs');
+if (fs.existsSync("./bot_token.txt")) {
+	if (fs.statSync('./bot_token.txt').size == 0) {
+		console.log("./bot_token.txt file is empty")
+	} else {
+		console.log("bot_token.txt file is not empty")
+		bot_token = fs.readFileSync('./bot_token.txt','utf8');
+	}
+	
+} else {
+	
+	const stream = fs.createWriteStream('./bot_token.txt');
+	stream.write("");
+	stream.end();
+}
+
+console.log("Bot token entered: ", bot_token);
+if (bot_token == undefined) process.exit(0);
+
+client.loginBot(bot_token);
